6816987d2e97fe0fb79e599fb4b13282500c2a94
Andrew Druffner <andrew@nomstock.com>
2015-01-30 00:28:08 -0800

    removed passwords after changing them. added separate change-password.sql script so we can treat it separately and assume-unchanged rather than all sql files

--- config/wp-config-db.php
+++ config/wp-config-db.php
@@ -1,12 +1,12 @@
 <?php
 	
-define('DB_NAME', 'clients_glandscapedesign_wp');
+define('DB_NAME', 'YOUR_DB_NAME_HERE');
 
 /** MySQL database username */
-define('DB_USER', 'admin_clients');
+define('DB_USER', 'YOUR_DB_USER_HERE');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Ai5$dXBJJ7#JH8XY');
+define('DB_PASSWORD', 'YOUR_DB_PASSWORD_HERE');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
